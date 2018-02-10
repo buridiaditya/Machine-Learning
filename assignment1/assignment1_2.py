16f27d0853c91b68dfd3f841b6bb3868e74e36dc
Buridi Aditya <buridiaditya@iitkgp.ac.in>
2018-02-11 02:02:25 +0530

    Bug fixes in parts 2 and 3

--- assignment1/assignment1_2.py
+++ assignment1/assignment1_2.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as py
+import matplotlib.pyplot as pl
 import csv
 import pandas as pd
 
@@ -15,42 +15,29 @@ class LinearRegression:
 
     def L2Cost(self, X, y_target,reg):
         cost = 0.0
-        M,N = X.shape
-        grads = np.array(self.W.shape)
+        N,M = X.shape
+        grads = np.ones(self.W.shape)
         scores = X.dot(self.W)
         cost = np.sum(np.square(scores - y_target))/N + reg * np.sum(self.W*self.W)
         grads = 2*X.T.dot(scores - y_target)/N + 2*self.W*reg
         return cost, grads
 
-    def trainSGD(self,X_train, y_target,X_test,y_test, epochs=200, learning_rate=1e-4,lr_decay = 0.95,reg = 0.0,batch_size=None):
-
+    def trainSGD(self,X_train, y_target,X_test,y_test, epochs=1000, learning_rate=0.05,lr_decay = 1,reg = 0.0,batch_size=None):
         N,M = X_train.shape
         self.W = np.random.randn(M)*0.01
-
-        if batch_size is None:
-            batch_size = N
-
         old_cost = 0.0
+        cost_data = []
         for i in range(epochs):
-            # To prevent overfitting of data
-            if batch_size is None:
-                X_batch = X_train
-                y_batch = y_target
-            else:
-                index = np.random.choice(N,batch_size)
-                X_batch = X_train[index]
-                y_batch = y_target[index]
-            no_of_iterations_per_epoch = int(N/batch_size)
-            cost=None
-            for j in range(no_of_iterations_per_epoch):
-                cost, dW = self.L2Cost(X_batch,y_batch,reg)
-                self.W = self.W - learning_rate*dW
+            cost, dW = self.L2Cost(X_train,y_target,reg)
+            self.W = self.W - learning_rate*dW
+            cost_data.append(cost)
             print("Cost after %d epochs : %f" %(i,cost))
             print("Cost difference %f" %(np.abs(cost-old_cost)) )
             old_cost = cost
-            if i%10 == 0:
+            if i%100 == 0:
                 learning_rate *= lr_decay
-            print("\nAccuracy after %d epochs : %f\n" %(i,np.sqrt(np.sum(np.square(self.predict(X_test)-y_test))/N)) )
+                print("\nAccuracy after %d epochs : %f\n" %(i,np.sqrt(np.sum(np.square(self.predict(X_test)-y_test))/N)) )
+        return cost_data
 
     def trainIRLS(self,X_train, y_target,X_test,y_test, reg = 0.0):
 
@@ -74,23 +61,6 @@ y_target = X_y[:,M-1]
 print("Shape of X : ",X.shape)
 print("Shape of y_target : ",y_target.shape)
 
-# Data Preprocessing
-# Zero centering and  Normalizing data
-X_mean = np.mean(X,axis=0)
-X_max = np.max(X,axis=0)
-X_min = np.min(X,axis=0)
-X -= X_mean
-X_std = np.std(X,axis=0)
-#X /= (X_max-X_min)
-X/=X_std
-
-# append a column of ones to X
-N,M = X.shape
-X_temp = np.ones((N,M+1))
-M += 1
-X_temp[:,1:M] = X
-X = X_temp
-
 # Split data into train and test data
 #
 size_of_train = int(N*0.8)
@@ -102,6 +72,31 @@ print("Shape of X_train : ", X_train.shape)
 print("Shape of X_test : ", X_test.shape)
 print("Shape of y_train : ", y_train.shape)
 print("Shape of y_test : ", y_test.shape)
+
+# Data Preprocessing
+# Zero centering and  Normalizing data
+X_mean = np.mean(X_train,axis=0)
+X_max = np.max(X_train,axis=0)
+X_min = np.min(X_train,axis=0)
+X_train -= X_mean
+X_std = np.std(X,axis=0)
+#X /= (X_max-X_min)
+X_train /=X_std
+X_test -= X_mean
+X_test /= X_std
+
+# append a column of ones to X
+N,M = X_train.shape
+X_temp = np.ones((N,M+1))
+M += 1
+X_temp[:,1:M] = X_train
+X_train = X_temp
+
+N,M = X_test.shape
+X_temp = np.ones((N,M+1))
+M += 1
+X_temp[:,1:M] = X_test
+X_test = X_temp
 
 model = LinearRegression()
 
--- assignment1/assignment1_3.py
+++ assignment1/assignment1_3.py
@@ -13,43 +13,29 @@ class LinearRegression:
         y_predict = X.dot(self.W)
         return y_predict
 
-
     def L2Cost(self, X, y_target,reg):
         cost = 0.0
-        M,N = X.shape
-        grads = np.array(self.W.shape)
+        N,M = X.shape
+        grads = np.zeros(self.W.shape)
         scores = X.dot(self.W)
         cost = np.sum(np.square(scores - y_target))/N + reg * np.sum(self.W*self.W)
-        reg_ = self.W
         grads = 2*X.T.dot(scores - y_target)/N + 2*self.W*reg
         return cost, grads
 
-    def train(self,X_train, y_target,X_test,y_test, epochs=10000, learning_rate=1e-4,lr_decay = 0.93,reg = 0.0,batch_size=None):
+    def train(self,X_train, y_target,X_test,y_test, epochs=50000, learning_rate=0.05,lr_decay = 0.97,reg = 0.0,batch_size=None):
         N,M = X_train.shape
-        #self.W = np.random.randn(M)*100
-        self.W = np.zeros(M)
+        self.W = np.random.randn(M)
+        #self.W = np.zeros(M)
         cost_data = []
         old_cost = 0.0
         for i in range(epochs):
-            # To prevent overfitting of data
-            if batch_size is None:
-                X_batch = X_train
-                y_batch = y_target
-                batch_size = N
-            else:
-                index = np.random.choice(N,batch_size)
-                X_batch = X_train[index]
-                y_batch = y_target[index]
-            no_of_iterations_per_epoch = int(N/batch_size)
-            cost=None
-            for j in range(no_of_iterations_per_epoch):
-                cost, dW = self.L2Cost(X_batch,y_batch,reg)
-                self.W = self.W - learning_rate*dW
+            cost, dW = self.L2Cost(X_train,y_train,reg)
+            self.W = self.W - learning_rate*dW
             print("Cost after %d epochs : %f" %(i,cost))
             print("Cost difference %f" %(np.abs(cost-old_cost)) )
-            cost_data.append(math.fabs(cost-old_cost))
+            cost_data.append(cost)
             old_cost = cost
-            if i%10 == 0:
+            if i%100 == 0:
                 learning_rate *= lr_decay
                 print("\nAccuracy after %d epochs : %f\n" %(i,np.sqrt(np.sum(np.square(self.predict(X_test)-y_test))/N)) )
         return cost_data
@@ -65,8 +51,6 @@ y_target = X_y[:,M-1]
 print("Shape of X : ",X.shape)
 print("Shape of y_target : ",y_target.shape)
 
-#print(X)
-#print(y_target)
 # Adds quadratic cubic features
 N,M = X.shape
 X_temp = np.ones((N,3*M))
@@ -75,27 +59,9 @@ X_temp[:,M:2*M] = X*X
 X_temp[:,2*M:3*M] = X*X*X
 X = X_temp
 
-# Data Preprocessing
-# Zero centering and  Normalizing data
-X_mean = np.mean(X,axis=0)
-X_max = np.max(X,axis=0)
-X_min = np.min(X,axis=0)
-X -= X_mean
-X_std = np.std(X,axis=0)
-#X /= (X_max-X_min)
-X/=X_std
-
-
-# Append column of ones to X
-N,M = X.shape
-X_temp = np.ones((N,M+1))
-M += 1
-X_temp[:,1:M] = X
-X = X_temp
-
 # Split data into train and test data
 #
-size_of_train = int(N*0.9)
+size_of_train = int(N*0.8)
 X_train = X[0:size_of_train]
 X_test = X[size_of_train:N]
 y_train = y_target[0:size_of_train]
@@ -104,6 +70,33 @@ print("Shape of X_train : ", X_train.shape)
 print("Shape of X_test : ", X_test.shape)
 print("Shape of y_train : ", y_train.shape)
 print("Shape of y_test : ", y_test.shape)
+
+# Data Preprocessing
+# Zero centering and  Normalizing data
+X_mean = np.mean(X_train,axis=0)
+X_max = np.max(X_train,axis=0)
+X_min = np.min(X_train,axis=0)
+X_std = np.std(X_train,axis=0)
+
+X_train -= X_mean
+#X_train /= (X_max-X_min)
+X_train /= X_std
+X_test -= X_mean
+X_test /= X_std
+#X_test /= (X_max-X_min)
+
+# Append column of ones to X
+N,M = X_train.shape
+X_temp = np.ones((N,M+1))
+M += 1
+X_temp[:,1:M] = X_train
+X_train = X_temp
+
+N,M = X_test.shape
+X_temp = np.ones((N,M+1))
+M += 1
+X_temp[:,1:M] = X_test
+X_test = X_temp
 
 model = LinearRegression()
 
