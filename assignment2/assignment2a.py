3072de3c3db3f814e0a588ead3acbe640e4f8426
Buridi Aditya <buridiaditya@gmail.com>
2018-04-08 18:46:51 +0530

    Updates preprocessing and makes utility functions of neural networks

--- assignment2/assignment2a.py
+++ assignment2/assignment2a.py
@@ -1,36 +1,58 @@
 import numpy as np
 import re
 import nltk
-nltk.download()
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 # Read file in lines
 fd = open("Assignment_2_data.txt","r")
 file_data = fd.read()
 lines = (file_data).splitlines()
 
+ps = PorterStemmer()
+
 y_target_names = []
 x = []
 
 stop_words = set(stopwords.words('english'))
-# Pasing input file into labels and features
-for i in range(len(lines)):
-	words = re.split('\W+',lines[i])
-	words = list(filter(lambda temp: (temp != '' ),words))
-	words = [w for w in words if not w in stop_words]
-	#words = lines[i].split()
-	y_target_names.append(words[0])
-	x.append(words[1:])
-	#print (x[i])
+unique_tokens = set()
+# Pasing input file into labels and features 
+for i in range(len(lines)): 
+	# Splitting based on white space and non alpha numberic characters
+	words_tokens = re.split('\W+',lines[i])
+	words_tokens = list(filter(lambda temp: (temp != '' ),words_tokens)) 
 	
-# Creating One hot encoding of the data
-tokens = set([val for sublist in x for val in sublist])
-print (tokens)
+	# Removing stop word from the data
+	filtered_sentence = []
+	for w in words_tokens:
+		if w not in stop_words:
+		# Applying porter stemmer
+			stemmed_word = ps.stem(w).lower()
+			filtered_sentence.append(stemmed_word)
+			
 
+	# Store pre processed data 
+	y_target_names.append(filtered_sentence[0])
+	x.append(filtered_sentence[1:])
+	unique_tokens.update(filtered_sentence[1:])
+#print (y_target_names)
+	
+# One hot encoding.Note: Does not give importance to the order in which the words occur.
+N = len(x)
+unique_tokens = list(unique_tokens)
+encoding_length = (len(unique_tokens))
+x_encoded = np.zeros( ( N , encoding_length) )
 
-# assuming one hot encoding available
-N, M = X.shape
+# Binary encoding 
+for i in range(N):
+	for j in x[i]:
+		ind = unique_tokens.index(j)
+		x_encoded[i][ind] = 1;
 
+y_target = np.zeros(N)
+for i in range(len(y_target_names)):
+	if(y_target_names[i] == "ham"):
+		y_target[i] = 1
 
-
+# Training the model
 
--- assignment2/neuralnetwork.py
+++ assignment2/neuralnetwork.py
@@ -1,4 +1,6 @@
-class NeuralNetworks(object):
+import numpy as np
+
+class NeuralNetwork(object):
 	def __init__(self,input_dim,hidden_dims,num_classes,activation_function,weight_scale=10e-2):
 		L = hidden_dims.shape
 		self.params = {}
@@ -22,7 +24,56 @@ class NeuralNetworks(object):
 		return value
 
 	def act_tanh(self,X):
-		
+		value = np.tanh(X)
+		return value
 
-	def loss(self,X,y_target):
-		for i in 
+	def act_sigmoid(self,X):
+		value = 1/(1+np.exp(-X))
+		return value
+
+	def dtanh(self,dX,out):
+		return dX*(1 - np.square(out))
+
+	def dsigmoid(self,dX,out):
+		return dX*(out)*(1-out)
+
+	def dlinear(self,dX,W,X):
+		dW = X.T.dot(dX)
+		dx = dX.dot(W.T)
+		db = np.ones(X.shape[0]).dot(dX)
+		return dW, db, dx
+
+	def loss_1(self,X,y_target):
+		N,_  = X.shape
+		X_i = X
+		backup = []
+		for i in range(self.hidden_layers + 1):
+			indexW = 'W' + str(i)
+			indexb = 'b' + str(i)	
+			W = self.params[indexW] 
+			b = self.params[indexb] 
+			X_old = X_i
+			X_i = linear(X_i,W,b)
+			# Use any activation fucntion
+			X_i = act_tanh(X_i)
+			backup.append((X_old,X_i))
+			# X_i = act_sigmoid(X_i)
+
+		# For sigmoid activation function
+		# y_target[y_target == 0] = -1
+		scores = X_i
+		cost = np.sum(np.square(X_i-y_target))/N
+		self.grads = {}
+		dX = 2*(scores-y_target)/N
+		for i in xrange(L+1,0,-1):
+			indexW = 'W' + str(i-1)
+			indexb = 'b' + str(i-1)
+			x_old,x_in = backup[i]
+			dX = dtanh(dX,x_in)
+			self.grads[indexW], self.grads[indexb], dX = dlinear(dX,params[indexW],x_old) 
+
+		return cost
+
+	
+			
+		
