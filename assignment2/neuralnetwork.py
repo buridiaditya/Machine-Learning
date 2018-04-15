30e915d7bc04bc8c2aab00ba59c7a90f487c2682
Buridi Aditya <buridiaditya@gmail.com>
2018-04-15 20:29:31 +0530

    Bug fixes

--- assignment2/neuralnetwork.py
+++ assignment2/neuralnetwork.py
@@ -16,13 +16,13 @@ class NeuralNetwork(object):
 			indexb = 'b' + str(i)
 			if i == 0:
 				self.params[indexW] = np.random.normal(0,weight_scale,(input_dim,hidden_dims[i]))
-				self.params[indexb] = np.random.normal(0,weight_scale,(hidden_dims[i]))
+				self.params[indexb] = np.random.normal(0,weight_scale,(1,hidden_dims[i]))
 			elif i == L:
 				self.params[indexW] = np.random.normal(0,weight_scale,(hidden_dims[i-1],num_classes))
-				self.params[indexb] = np.random.normal(0,weight_scale,(num_classes))
+				self.params[indexb] = np.random.normal(0,weight_scale,(1,num_classes))
 			else :
 				self.params[indexW] = np.random.normal(0,weight_scale,(hidden_dims[i-1],hidden_dims[i]))
-				self.params[indexb] = np.random.normal(0,weight_scale,(hidden_dims[i]))
+				self.params[indexb] = np.random.normal(0,weight_scale,(1,hidden_dims[i]))
 
 	def linear(self,X,W,b):
 		value = X.dot(W) + b
@@ -50,49 +50,50 @@ class NeuralNetwork(object):
 
 	def loss_1(self,X,y_target,predict=False):
 		N  = X.shape[0]
-		X_i = X
+		X_i = np.copy(X)
 		backup = []
-		self.grads = {}
 		# Forward Pass
 		for i in range(self.hidden_layers + 1):
 			indexW = 'W' + str(i)
 			indexb = 'b' + str(i)
 			W = self.params[indexW]
 			b = self.params[indexb]
-			X_old = X_i.copy()
+			X_old = np.copy(X_i)
 
 			# Affine function
 			X_i = self.linear(X_i,W,b)
 			# Activation function
 			if(self.activation == "sigmoid"):
 				X_i = self.act_sigmoid(X_i)
-			elif(self.activation == "tanh"):
+			else:
 				X_i = self.act_tanh(X_i)
 			# Store node data for use in backward pass
-			backup.append((X_old,X_i))
-
-		scores = X_i.copy()
+			backup.append( (np.copy(X_old), np.copy(X_i) ) )
+ 
+		scores = np.copy(X_i)
 		if(predict == True):
 			return scores
 
-		y_target_ = y_target.copy()
+		self.grads = {}
+
+		y_target_ = np.copy(y_target)
 		# For tanh activation function the labels of 0 replaced with -1
 		if(self.activation == "tanh"):
 			y_target_[y_target_ == 0] = -1
 
 
 		# L2 error
-		cost = np.sum(np.square(X_i-y_target_))/N
+		cost = np.sum(np.square(X_i-y_target_))
 
 		# Backward Pass
-		dX = 2*(scores-y_target_)/N
+		dX = 2*(scores-y_target_)
 		for i in range(self.hidden_layers+1,0,-1):
 			indexW = 'W' + str(i-1)
 			indexb = 'b' + str(i-1)
 			x_old,x_in = backup[i-1]
 			if(self.activation == "sigmoid"):
 				dX = self.dsigmoid(dX,x_in)
-			elif(self.activation == "tanh"):
+			else:
 				dX = self.dtanh(dX,x_in)
 
 			self.grads[indexW], self.grads[indexb], dX = self.dlinear(dX,self.params[indexW],x_old)
@@ -100,17 +101,17 @@ class NeuralNetwork(object):
 		return cost
 
 	def loss_2(self,X,y_target,predict=False):
-		N,_  = X.shape
-		X_i = X
+		N  = X.shape[0]
+		X_i = np.copy(X)
 		backup = []
-		self.grads = {}
+
 		# Forward Pass
 		for i in range(self.hidden_layers + 1):
 			indexW = 'W' + str(i)
 			indexb = 'b' + str(i)
 			W = self.params[indexW]
 			b = self.params[indexb]
-			X_old = X_i.copy()
+			X_old = np.copy(X_i)
 
 			# Affine function
 			X_i = self.linear(X_i,W,b)
@@ -121,24 +122,25 @@ class NeuralNetwork(object):
 				elif(self.activation == "tanh"):
 					X_i = self.act_tanh(X_i)
 			# Store node data for use in backward pass
-			backup.append((X_old,X_i))
+			backup.append((np.copy(X_old),np.copy(X_i)))
 
-		scores = X_i.copy()
+		scores = np.copy(X_i)
 		temp = np.exp(X_i)
 		scores = temp / np.reshape(np.sum(temp,axis=1),(N,1))
 
-		scores_row1 = np.reshape(scores[:,0],(N,1)).copy()
-		scores_row2 = np.reshape(scores[:,1],(N,1)).copy()
+		scores_row1 = np.copy(np.reshape(scores[:,0],(N,1)))
+		scores_row2 = np.copy(np.reshape(scores[:,1],(N,1)))
 
 		if(predict == True):
 			return scores_row1
 
+		self.grads = {}
 		# L2 error
 		#Softmax function
-		cost = np.sum(np.square(scores_row1-y_target))/N
+		cost = np.sum(np.square(scores_row1-y_target))
 
 		# Backward Pass
-		dX = 2*(scores_row1-y_target)/N
+		dX = 2*(scores_row1-y_target)
 		#temp = scores
 		#temp[:,[0,1]]  = scores[:,[1, 0]]
 
@@ -189,7 +191,8 @@ class NeuralNetwork(object):
 	def train(self,X_train,y_train,X_test,y_test,epochs=100,learning_rate=1e-3,learning_rate_decay=0.95,batch_size=1000,method="one"):
 		N, M = X_train.shape
 		no_of_batches = int(N/batch_size)
-		#print(no_of_batches)
+		
+		# Create batches from training set
 		X_s = X_train[0:no_of_batches*batch_size]
 		X_s_t = X_train[no_of_batches*batch_size:]
 		X_batches = np.split(X_s,no_of_batches)
@@ -210,27 +213,47 @@ class NeuralNetwork(object):
 		self.method = method
 		
 		cost = 0.0
+		X_batches = np.array(X_batches)
+		y_batches = np.array(y_batches)
 		for i in range(epochs):
 			cost = 0.0
-			
-			if(i%25==0):
-				prediction = self.predict(X_test,y_test)
-				print("-----------Ratio of Correct predictions over testset(%d/%d)-----------"%(np.sum(prediction == y_test),y_test.shape[0]))
-				#print (prediction)
-				prediction = self.predict(X_train,y_train)
-				print("-----------Ratio of Correct predictions over training set(%d/%d)-----------"%(np.sum(prediction == y_train),y_train.shape[0]))
-
-			if((i+1)%25==0):
+			counter = no_of_batches+ ( 1 if(N%batch_size) else 0 )
+			indices = np.arange(counter).reshape(counter,1)
+			np.random.shuffle(indices)
+			X_batches = X_batches[indices,:]
+			y_batches = y_batches[indices,:]
+			# Decay learning rate after 20 epochs
+			if((i+1)%20==0):
 				learning_rate *= learning_rate_decay
-
+			
+			prediction = self.predict(X_test,y_test)				
+			outsample_cost = np.sum(np.square(prediction - y_test))/y_test.shape[0]
+			print("-----------Ratio of Correct predictions over testset(%d/%d)-----------"%(np.sum(prediction == y_test),y_test.shape[0]))
+			
+			# Compute Train accuracy
+			prediction = self.predict(X_train,y_train)
+			insample_cost = np.sum(np.square(prediction - y_train))/y_train.shape[0]
+			print("-----------Ratio of Correct predictions over training set(%d/%d)-----------"%(np.sum(prediction == y_train),y_train.shape[0]))
+			
+			# Store insample and out sample error for plotting
+			self.no_epochs.append(i)
+			self.square_error_train.append(insample_cost)
+			self.square_error_test.append(outsample_cost)
+			
 			for j in range(no_of_batches+ ( 1 if(N%batch_size) else 0 ) ):
-				#print (X_batches[j].shape)
+				# Compute Test accuracy and out sample error
+				
 				temp1 = 0.0
+				# Decide which neural network architecture to use
 				if(method == "two"):
 					temp1 = self.loss_2(X_batches[j],y_batches[j])
 				else:
 					temp1 = self.loss_1(X_batches[j],y_batches[j])
+			
+				# Compute cummilative cost for showing per epoch cost
 				cost += temp1
+			
+				# Update weights of the neural network
 				for k in range(self.hidden_layers + 1):
 					indexW = 'W' + str(k)
 					indexb = 'b' + str(k)
@@ -238,19 +261,20 @@ class NeuralNetwork(object):
 					b = self.params[indexb]
 					self.params[indexW] = W - learning_rate*self.grads[indexW]
 					self.params[indexb] = b - learning_rate*self.grads[indexb]
-
+									
 			print("Epoch (%d/%d) Training Error : %f"%(i+1,epochs,cost/( no_of_batches+ ( 1 if(N%batch_size) else 0 ) ) ) )
-
-			prediction = self.predict(X_test,y_test)
-			outsample_cost = np.sum(np.square(prediction - y_test))/y_test.shape[0]
-			self.no_epochs.append(i)
-			self.square_error_train.append(cost)
-			self.square_error_test.append(outsample_cost)
-			if(cost/( no_of_batches+ ( 1 if(N%batch_size) else 0 )) < 0.005):
+			
+			# If error is less than 0.005 stop training
+			if(cost/( no_of_batches+ ( 1 if(N%batch_size) else 0 )) < 0.015):
 				break;
 
 		prediction = self.predict(X_test,y_test)
-		#print(prediction)
+		outsample_cost = np.sum(np.square(prediction - y_test))/y_test.shape[0]
 		print("-----------Ratio of Correct predictions over testset(%d/%d)-----------"%(np.sum(prediction == y_test),y_test.shape[0]))
 		prediction = self.predict(X_train,y_train)
+		insample_cost = np.sum(np.square(prediction - y_train))/y_train.shape[0]
 		print("-----------Ratio of Correct predictions over training set(%d/%d)-----------"%(np.sum(prediction == y_train),y_train.shape[0]))
+		self.no_epochs.append(i)
+		self.square_error_train.append(insample_cost)
+		self.square_error_test.append(outsample_cost)
+
